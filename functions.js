770cf13c0f4c2c633015295318e34128f10b4269
pawankalyan <pawankalyankarri22@gmail.com>
2025-09-09 21:48:21 +0530

    different types of functions are added

---
+++ functions.js
@@ -0,0 +1,37 @@
+        //anonymous function
+        //automatically called this function
+
+// (function (){
+//     console.log('hi')
+// })()
+
+
+            // rest operator in function
+
+
+// function addition(...x){
+//     console.log(x)
+//     console.log(x.reduce((a,b)=>a+b))
+    
+// }
+// addition(1,2,3,4,5,6,7,8,9)
+
+            //arrow functions
+
+// let fn = (a,b)=>a+b
+// console.log(fn(4,5))
+
+        // this keyword
+
+
+// function display(){
+//     console.log(this)  // here this refers the window object or global
+// }
+// display()
+
+
+// let fn = ()=>{
+//     console.log(this)
+// }
+// fn()
+
